fc030d38a2d7983e8b0789b4ac5d706de964b049
Edge Y. Zhang <edgeyyzhang@gmail.com>
2023-03-02 21:10:34 -0500

    v1.3.1 auto detect dir

--- src/extension.ts
+++ src/extension.ts
@@ -12,23 +12,24 @@ export function activate(context: vscode.ExtensionContext) {
             let name_cur = doc_cur.fileName;
             let text_cur = doc_cur.getText();
             let base_cur = name_cur.substring(name_cur.lastIndexOf("/")+1, name_cur.length);
+            let dir_cur = name_cur.substring(0,name_cur.lastIndexOf("/")+1);
 
             // Write Research
             if (/### Res_TBD\n./.test(text_cur)){
                 let str_write = '![['+base_cur+'#Res_TBD]]'
 
                 let fs = require('fs');
-                let file_res = fs.readFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.res.md', 'utf8')
+                let file_res = fs.readFileSync(dir_cur+'daily.res.md', 'utf8')
                 let text_res = file_res.split("---\n")[2].split("\n\n").slice(0,-1)
 
                 if (!text_res.includes(str_write) && str_write>text_res[text_res.length-1]){
-                    fs.appendFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.res.md', str_write+'\n\n');
+                    fs.appendFileSync(dir_cur+'daily.res.md', str_write+'\n\n');
                 } else if (!text_res.includes(str_write)){
                     let head_res = "---\n"+file_res.split("---\n")[1]+"---\n"
                     text_res.push(str_write)
                     text_res.sort()
                     let final = head_res + text_res.join("\n\n") + "\n\n"
-                    fs.writeFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.res.md', final);   
+                    fs.writeFileSync(dir_cur+'daily.res.md', final);   
                 }
             }
 
@@ -38,17 +39,17 @@ export function activate(context: vscode.ExtensionContext) {
                 let str_write = '![['+base_cur+'#Triv_TBD]]'
 
                 let fs = require('fs');
-                let file_triv = fs.readFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.triv.md', 'utf8')
+                let file_triv = fs.readFileSync(dir_cur+'daily.triv.md', 'utf8')
                 let text_triv = file_triv.split("---\n")[2].split("\n\n").slice(0,-1)
 
                 if (!text_triv.includes(str_write) && str_write>text_triv[text_triv.length-1]){
-                    fs.appendFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.triv.md', str_write+'\n\n');
+                    fs.appendFileSync(dir_cur+'daily.triv.md', str_write+'\n\n');
                 } else if (!text_triv.includes(str_write)){
                     let head_triv = "---\n"+file_triv.split("---\n")[1]+"---\n"
                     text_triv.push(str_write)
                     text_triv.sort()
                     let final = head_triv + text_triv.join("\n\n") + "\n\n"
-                    fs.writeFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.triv.md', final);   
+                    fs.writeFileSync(dir_cur+'daily.triv.md', final);   
                 }
             }
 
@@ -57,17 +58,17 @@ export function activate(context: vscode.ExtensionContext) {
                 let str_write = '![['+base_cur+'#Meet_TBD]]'
 
                 let fs = require('fs');
-                let file_meet = fs.readFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.meet.md', 'utf8')
+                let file_meet = fs.readFileSync(dir_cur+'daily.meet.md', 'utf8')
                 let text_meet = file_meet.split("---\n")[2].split("\n\n").slice(0,-1)
 
                 if (!text_meet.includes(str_write) && str_write>text_meet[text_meet.length-1]){
-                    fs.appendFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.meet.md', str_write+'\n\n');
+                    fs.appendFileSync(dir_cur+'daily.meet.md', str_write+'\n\n');
                 } else if (!text_meet.includes(str_write)){
                     let head_meet = "---\n"+file_meet.split("---\n")[1]+"---\n"
                     text_meet.push(str_write)
                     text_meet.sort()
                     let final = head_meet + text_meet.join("\n\n") + "\n\n"
-                    fs.writeFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.meet.md', final);   
+                    fs.writeFileSync(dir_cur+'daily.meet.md', final);   
                 }
             }
 
@@ -89,26 +90,27 @@ export function activate(context: vscode.ExtensionContext) {
             let doc_cur = editor.document;
             let name_cur = doc_cur.fileName;
             let base_cur = name_cur.substring(name_cur.lastIndexOf("/")+1, name_cur.length);
+            let dir_cur = name_cur.substring(0,name_cur.lastIndexOf("/")+1);
             
             
             // Remove from Research
             let str_rmv = '![['+base_cur+'#Res_TBD]]\n\n'
             let fs = require('fs');
-            let file_res = fs.readFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.res.md', 'utf8')
+            let file_res = fs.readFileSync(dir_cur+'daily.res.md', 'utf8')
             file_res = file_res.replace(str_rmv, '')
-            fs.writeFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.res.md', file_res);   
+            fs.writeFileSync(dir_cur+'daily.res.md', file_res);   
 
             // Remove from Triviality
             str_rmv = '![['+base_cur+'#Triv_TBD]]\n\n'
-            let file_triv = fs.readFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.triv.md', 'utf8')
+            let file_triv = fs.readFileSync(dir_cur+'daily.triv.md', 'utf8')
             file_triv = file_triv.replace(str_rmv, '')
-            fs.writeFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.triv.md', file_triv);   
+            fs.writeFileSync(dir_cur+'daily.triv.md', file_triv);   
 
             // Remove from Meeting
             str_rmv = '![['+base_cur+'#Meet_TBD]]\n\n'
-            let file_meet = fs.readFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.meet.md', 'utf8')
+            let file_meet = fs.readFileSync(dir_cur+'daily.meet.md', 'utf8')
             file_meet = file_meet.replace(str_rmv, '')
-            fs.writeFileSync('/Users/edge/Documents/EdgeExplore/98_Notes/notes/daily.meet.md', file_meet);   
+            fs.writeFileSync(dir_cur+'daily.meet.md', file_meet);   
 
             vscode.window.showInformationMessage('Successfully Removed Daily!');      
     }}
