726febdd0e20c6f21507c3d3aa1fbf20102f585c
Edge Y. Zhang <edgeyyzhang@gmail.com>
2023-03-29 15:12:53 -0400

    transfer to foam

--- out/extension.js
+++ out/extension.js
@@ -29,32 +29,34 @@ function activate(context) {
             // Get Section Settings (sections)
             let doc_cur = editor.document;
             let resource = doc_cur.uri;
-            let config = vscode.workspace.getConfiguration('edgeDH', resource);
+            let config = vscode.workspace.getConfiguration('edgeFH', resource);
             let secconf = config.get('dailySections');
             if (typeof secconf === 'string') {
-                vscode.window.showInformationMessage(secconf);
                 let sections = secconf.split(", ");
                 // Get Current Document Text
                 let name_cur = doc_cur.fileName;
                 let text_cur = doc_cur.getText();
                 let base_cur = name_cur.substring(name_cur.lastIndexOf("/") + 1, name_cur.length);
+                base_cur = base_cur.substring(0, base_cur.lastIndexOf('.'));
                 let dir_cur = name_cur.substring(0, name_cur.lastIndexOf("/") + 1);
                 for (var sec of sections) {
-                    // Write One Section
-                    if (RegExp(sec + "\n.").test(text_cur)) {
-                        let str_write = '![[' + base_cur + '#' + sec + ']]';
+                    // Write One Section            
+                    vscode.window.showInformationMessage(sec + " (" + base_cur + ")\n.");
+                    if (RegExp(sec + " \\(" + base_cur + "\\)\n.").test(text_cur)) {
+                        vscode.window.showInformationMessage(base_cur);
+                        let str_write = '![[' + base_cur + '#' + sec + ' (' + base_cur + ')' + ']]';
+                        let file_write = dir_cur + "../journal." + sec.toLowerCase().replace(' ', '-') + '.md';
                         let fs = require('fs');
-                        let file_res = fs.readFileSync(dir_cur + 'daily.' + sec.toLowerCase() + '.md', 'utf8');
-                        let text_res = file_res.split("---\n")[2].split("\n\n").slice(0, -1);
+                        let file_res = fs.readFileSync(file_write, 'utf8');
+                        let text_res = file_res.split("\n\n").slice(0, -1);
                         if (!text_res.includes(str_write) && str_write > text_res[text_res.length - 1]) {
-                            fs.appendFileSync(dir_cur + 'daily.' + sec.toLowerCase() + '.md', str_write + '\n\n');
+                            fs.appendFileSync(file_write, str_write + '\n\n');
                         }
                         else if (!text_res.includes(str_write)) {
-                            let head_res = "---\n" + file_res.split("---\n")[1] + "---\n";
                             text_res.push(str_write);
                             text_res.sort();
-                            let final = head_res + text_res.join("\n\n") + "\n\n";
-                            fs.writeFileSync(dir_cur + 'daily.' + sec.toLowerCase() + '.md', final);
+                            let final = text_res.join("\n\n") + "\n\n";
+                            fs.writeFileSync(file_write, final);
                         }
                     }
                 }
@@ -66,7 +68,7 @@ function activate(context) {
         }
     };
     // Register Command
-    let command = vscode.commands.registerCommand("edgeDH.syncDaily", syncDaily);
+    let command = vscode.commands.registerCommand("edgeFH.syncDaily", syncDaily);
     context.subscriptions.push(command);
     let removeDaily = async () => {
         let editor = vscode.window.activeTextEditor;
@@ -74,10 +76,9 @@ function activate(context) {
             // Get Section Settings
             let doc_cur = editor.document;
             let resource = doc_cur.uri;
-            let config = vscode.workspace.getConfiguration('edgeDH', resource);
+            let config = vscode.workspace.getConfiguration('edgeFH', resource);
             let secconf = config.get('dailySections');
             if (typeof secconf === 'string') {
-                vscode.window.showInformationMessage(secconf);
                 let sections = secconf.split(", ");
                 // Get the document text
                 let name_cur = doc_cur.fileName;
@@ -96,7 +97,7 @@ function activate(context) {
         }
     };
     // Register Command
-    command = vscode.commands.registerCommand("edgeDH.removeDaily", removeDaily);
+    command = vscode.commands.registerCommand("edgeFH.removeDaily", removeDaily);
     context.subscriptions.push(command);
 }
 exports.activate = activate;
--- src/extension.ts
+++ src/extension.ts
@@ -10,34 +10,37 @@ export function activate(context: vscode.ExtensionContext) {
             // Get Section Settings (sections)
             let doc_cur = editor.document;
             let resource = doc_cur.uri;
-            let config = vscode.workspace.getConfiguration('edgeDH', resource);
+            let config = vscode.workspace.getConfiguration('edgeFH', resource);
             let secconf = config.get('dailySections');
             if (typeof secconf === 'string') {
                 let sections = secconf.split(", ")
-        
+                
                 // Get Current Document Text
                 let name_cur = doc_cur.fileName;
                 let text_cur = doc_cur.getText();
                 let base_cur = name_cur.substring(name_cur.lastIndexOf("/")+1, name_cur.length);
+                base_cur = base_cur.substring(0,base_cur.lastIndexOf('.'));
                 let dir_cur = name_cur.substring(0,name_cur.lastIndexOf("/")+1);
-
+                
                 for (var sec of sections){
-                    // Write One Section
-                    if (RegExp(sec+"\n.").test(text_cur)){
-                        let str_write = '![['+base_cur+'#'+sec+']]'
+                    // Write One Section            
+                    vscode.window.showInformationMessage(sec+" ("+base_cur+")\n.")   
+                    if (RegExp(sec+" \\("+base_cur+"\\)\n.").test(text_cur)){
+                        vscode.window.showInformationMessage(base_cur)
+                        let str_write = '![['+base_cur+'#'+sec+' ('+base_cur+')'+']]'
+                        let file_write = dir_cur+"../journal."+sec.toLowerCase().replace(' ','-')+'.md'
 
                         let fs = require('fs');
-                        let file_res = fs.readFileSync(dir_cur+'daily.'+sec.toLowerCase()+'.md', 'utf8')
-                        let text_res = file_res.split("---\n")[2].split("\n\n").slice(0,-1)
+                        let file_res = fs.readFileSync(file_write, 'utf8')
+                        let text_res = file_res.split("\n\n").slice(0,-1)
 
                         if (!text_res.includes(str_write) && str_write>text_res[text_res.length-1]){
-                            fs.appendFileSync(dir_cur+'daily.'+sec.toLowerCase()+'.md', str_write+'\n\n');
+                            fs.appendFileSync(file_write, str_write+'\n\n');
                         } else if (!text_res.includes(str_write)){
-                            let head_res = "---\n"+file_res.split("---\n")[1]+"---\n"
                             text_res.push(str_write)
                             text_res.sort()
-                            let final = head_res + text_res.join("\n\n") + "\n\n"
-                            fs.writeFileSync(dir_cur+'daily.'+sec.toLowerCase()+'.md', final);   
+                            let final = text_res.join("\n\n") + "\n\n"
+                            fs.writeFileSync(file_write, final);   
                         }
                     }
                 }
@@ -50,7 +53,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
     // Register Command
     let command = vscode.commands.registerCommand(
-        "edgeDH.syncDaily",
+        "edgeFH.syncDaily",
         syncDaily
     )
     context.subscriptions.push(command)
@@ -62,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
             // Get Section Settings
             let doc_cur = editor.document;
             let resource = doc_cur.uri;
-            let config = vscode.workspace.getConfiguration('edgeDH', resource);
+            let config = vscode.workspace.getConfiguration('edgeFH', resource);
             let secconf = config.get('dailySections');
             
             if (typeof secconf === 'string') {
@@ -87,7 +90,7 @@ export function activate(context: vscode.ExtensionContext) {
     }}
     // Register Command
     command = vscode.commands.registerCommand(
-        "edgeDH.removeDaily",
+        "edgeFH.removeDaily",
         removeDaily
     )
     context.subscriptions.push(command)
